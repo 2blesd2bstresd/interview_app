6e29d5952639ccff65e2e2d36546e25e43b3e005
Maxwell Moneybags <tmax8200@gmail.com>
2015-05-26 13:42:04 -0700

    fix

--- config.py
+++ config.py
@@ -1,16 +1,17 @@
 # config.py
-
 import os
-
+# grabs the folder where the script runs
 basedir = os.path.abspath(os.path.dirname(__file__))
-
-DATABASE = 'd400u70sqnkvso'
-USERNAME = 'snvsvsufglzpal'
-PASSWORD = '-ReHAiJScBOaEUBlmqqJ8dBnQx'
-SECRET_KEY = 'my_precious'
+DATABASE = 'd400u70sqnkvso' 
+USERNAME = 'snvsvsufglzpal' 
+PASSWORD = '-ReHAiJScBOaEUBlmqqJ8dBnQx' 
 CSRF_ENABLED = True 
+SECRET_KEY = 'my_precious'
+
+# defines the full path for the database
 DATABASE_PATH = os.path.join(basedir, DATABASE)
 
+# DB URL
 URL = '//snvsvsufglzpal:-ReHAiJScBOaEUBlmqqJ8dBnQx@ec2-54-197-224-173.compute-1.amazonaws.com:5432/d400u70sqnkvso'
 
 # database URI
