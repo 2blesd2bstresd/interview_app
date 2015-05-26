1fb4c741950819376a4dfe5ceda6518befb67c96
Maxwell Moneybags <tmax8200@gmail.com>
2015-05-26 13:02:19 -0700

    fix

--- config.py
+++ config.py
@@ -8,10 +8,5 @@ database = 'd400u70sqnkvso'
 user = 'snvsvsufglzpal'
 pw = '-ReHAiJScBOaEUBlmqqJ8dBnQx'
 
-CSRF_ENABLED = True 
-# SECRET_KEY = 'my_precious'
-# defines the full path for the database
-DATABASE_PATH = os.path.join(basedir, DATABASE)
-
 # database URI
 SQLALCHEMY_DATABASE_URI = 'postgres://snvsvsufglzpal:-ReHAiJScBOaEUBlmqqJ8dBnQx@ec2-54-197-224-173.compute-1.amazonaws.com:5432/d400u70sqnkvso'
